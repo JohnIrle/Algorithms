17b5fbb9cd176927e223ef4d57cb79ebbf8b5e49
JohnIrle <irlejohn@gmail.com>
2020-08-06 22:15:21 -0500

    Initial Commit.

---
+++ javascript/binary-search.js
@@ -0,0 +1,21 @@
+const binarySearch = (array, item) => {
+    let low = 0;
+    let high = array.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = array[mid];
+
+        if (guess === item) {
+            return mid;
+        } else if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+module.exports = binarySearch;
---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+const binarySearch = require('./binary-search');
+
+const test_array = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(test_array, 3));
+console.log(binarySearch(test_array, -1));
